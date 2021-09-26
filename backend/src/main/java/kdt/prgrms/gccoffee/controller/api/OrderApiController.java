0d8e44e3805a83e8d83867c5bb32aea445e5c4be
0923kdh <0923kdh@gmail.com>
2021-09-26 19:29:35 +0900

    chore : Class 이름 변경 및 불필요 코드 제거

--- backend/src/main/java/kdt/prgrms/gccoffee/controller/api/OrderApiController.java
+++ backend/src/main/java/kdt/prgrms/gccoffee/controller/api/OrderApiController.java
@@ -4,17 +4,16 @@ import kdt.prgrms.gccoffee.dto.CreateOrderRequest;
 import kdt.prgrms.gccoffee.model.Email;
 import kdt.prgrms.gccoffee.model.Order;
 import kdt.prgrms.gccoffee.service.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OrderRestController {
+public class OrderApiController {
 
     private final OrderService orderService;
 
-    public OrderRestController(OrderService orderService) {
+    public OrderApiController(OrderService orderService) {
         this.orderService = orderService;
     }
 
